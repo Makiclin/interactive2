f0455de4cccf9a7d4f75aa71786b6bfe464e38d8
Marcus Fei <makiclin@gmail.com>
2018-11-01 14:40:11 -0700

    renamed

---
+++ PJ1/letterform-construction-marcus3.0/full.js
@@ -0,0 +1,46 @@
+$( document ).ready(function() {
+    $('#hoverArea').mousemove(function(e){
+
+        // adding code to get moust position inside of div, not the window / page
+        // get hoverArea's offset from the top left as you scroll
+        var parentOffset = $(this).offset();
+
+        // recalculate x and y mouse position
+        var xPos = e.pageX - parentOffset.left;
+        var yPos = e.pageY - parentOffset.top;
+
+        // var hoverArea = document.getElementById('hoverArea');
+        // hoverArea.style.background = 'hsl(2, 80%, dw)';
+
+
+        // update mouse x and y position
+        // let xPos = e.pageX;
+        // let yPos = e.pageY;
+        var windowWidth = $(window).width();
+
+        startValue = xPos-200;
+
+        if(xPos > 200 && xPos < windowWidth - 200){
+        $('.partSpeed2').css({
+            'left' : 0.1 * startValue + 34,
+        });
+
+        $('.partSpeed3').css({
+            'left' : 0.3 * startValue + 34,
+
+        });
+
+        $('.rootLetter').css({
+            'width' : 80 + 0.5 * startValue ,
+            // 'box-shadow':-4 0 22 0 ,
+            // 'margin-right' : 40 + 0.01 * startValue,
+
+        });
+
+
+      }
+
+
+    });
+
+});
